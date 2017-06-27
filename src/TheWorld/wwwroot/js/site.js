26dfe095fa06ca21abbedfb4e037c154fd2f9393
t4rn <tarn@onet.eu>
2017-06-27 11:28:43 +0200

    Bootstrap

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -22,13 +22,17 @@
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
 
+    var $icon = $("#sidebarToggleBtn i.fa");
+
     $("#sidebarToggleBtn").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
         }
         else {
-            $(this).text("Hide Sidebar");
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
         }
     });
 
