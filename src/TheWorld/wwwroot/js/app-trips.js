93acf5e78c7aeb72027c521975549bd6a46dc06d
t4rn <tarn@onet.eu>
2017-06-29 16:04:39 +0200

    AngularJS

---
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,9 @@
+﻿(function () {
+
+    "use strict";
+
+    // creating the module
+    angular.module("app-trips", ["simpleControls"]);
+
+
+})();
---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,56 @@
+﻿(function () {
+
+	"use strict";
+
+	// getting the existing module
+	angular.module("app-trips")
+	.controller("tripsController", tripsController);
+
+	function tripsController($http) {
+
+		var vm = this;
+
+		vm.trips = [];
+
+		vm.newTrip = {};
+
+		vm.errorMessage = "";
+		vm.isBusy = true;
+
+		$http.get("/api/trips")
+		.then(function (response) {
+			// success
+			angular.copy(response.data, vm.trips);
+		}, function (error) {
+			// failure
+			vm.errorMessage = "Failed to load data: " + error;
+		})
+		.finally(function () {
+			vm.isBusy = false;
+		});
+
+		vm.addTrip = function () {
+			
+			vm.isBusy = true;
+			vm.errorMessage = "";
+
+			$http.post("/api/trips", vm.newTrip)
+			.then(function (response) {
+				// success
+				vm.trips.push(response.data);
+				vm.newTrip = {};
+			}, function () {
+				// failure
+				vm.errorMessage = "Failed to save new trip";
+			})
+			.finally(function () {
+				vm.isBusy = false;
+			});
+
+			vm.trips.push({ name: vm.newTrip.name, dateCreated: new Date() });
+			vm.newTrip = {};
+		};
+
+	}
+
+})();
